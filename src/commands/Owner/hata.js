50c3b28dea7ea91353dfc1e26c4e6d2e3e1f688f
Xylus <105723951+Chinmay3110@users.noreply.github.com>
2026-06-10 23:11:09 +0530

    Update hata.js

--- src/commands/Owner/hata.js
+++ src/commands/Owner/hata.js
@@ -1,19 +1,16 @@
 /** @format
  *
- * Arrkiii By Ozuma xd
- * © 2024 Arrkiii Development
+ * Ryomen By Chinmay
+ * © 2026 Ryomen Development
  *
  */
 
 const {
   EmbedBuilder,
-  MessageFlags,
-  ButtonBuilder,
-  ButtonStyle,
   ActionRowBuilder,
   StringSelectMenuBuilder,
 } = require("discord.js");
-const NopAccess = require("../../schema/accessnop");
+
 const OxP1 = require("../../schema/noprefix");
 const OxP2 = require("../../schema/votebypassuser");
 const OxP3 = require("../../schema/badge");
@@ -26,9 +23,9 @@ module.exports = {
   args: false,
   usage: "",
   owner: false,
+
   execute: async (message, args, client, prefix) => {
-    const OzuMA = await NopAccess.findOne({ userId: message.author.id });
-    if (!OzuMA) {
+    if (message.author.id !== client.config.ownerID) {
       return message.channel.send("___You are not allowed to use this cmd!___");
     }
 
@@ -37,11 +34,13 @@ module.exports = {
 
     if (!targetUser) {
       return message.channel.send("Can you please mention a user to add?");
-    } else if (!args[0]) {
+    }
+
+    if (!args[1]) {
       const oxp = new EmbedBuilder()
         .setColor(client.color)
         .setDescription(
-          ` \`\`\`[] = Optional Argument\n<> = Required Argument\nDo NOT type these when using commands!)\`\`\`\n\n**Aliases:**\n\`\`[remove, -]\`\`\n**Usage:**\n\`\`nop/nov/bdg\`\``,
+          `\`\`\`[] = Optional Argument\n<> = Required Argument\nDo NOT type these when using commands!\`\`\`\n\n**Usage:**\n\`\`${prefix}hata @user nop\`\`\n\`\`${prefix}hata @user nov\`\`\n\`\`${prefix}hata @user bdg\`\``
         );
       return message.channel.send({ embeds: [oxp] });
     }
@@ -51,56 +50,69 @@ module.exports = {
         userId: targetUser.id,
         noprefix: true,
       });
-      if (!npData)
+
+      if (!npData) {
         return message.reply({
           content: `<:arrkiii:1187678838759628800> | This user is not present in my no prefix system.`,
         });
+      }
 
       await OxP1.deleteOne({ userId: targetUser.id, noprefix: true });
+
       return message.reply({
         embeds: [
           new EmbedBuilder()
             .setColor(client.color)
             .setDescription(
-              `${client.emoji.tick} | Successfully **Removed** ${targetUser} from my no prefix.`,
+              `${client.emoji.tick} | Successfully **Removed** ${targetUser} from my no prefix.`
             ),
         ],
       });
-    } else if (args[1] === "nov") {
+    }
+
+    if (args[1] === "nov") {
       const data = await OxP2.findOne({ userId: targetUser.id });
-      if (!data)
+
+      if (!data) {
         return message.channel.send({
           content: `This user is not in the vote bypass list.`,
         });
+      }
 
       await data.deleteOne();
+
       const embed = new EmbedBuilder().setDescription(
-        `Successfully removed ${targetUser} from the vote bypass list.`,
+        `Successfully removed ${targetUser} from the vote bypass list.`
       );
+
       return message.channel.send({ embeds: [embed] });
-    } else if (args[1] === "bdg") {
+    }
+
+    if (args[1] === "bdg") {
       const cache = [];
-      const selectedUser = message.mentions.users.first() || message.author;
+      const selectedUser = targetUser;
       const data = await OxP3.findOne({ userId: selectedUser.id });
 
-      if (data) {
-        for (const [badge, value] of Object.entries(data.badge)) {
-          if (value) {
-            cache.push(
-              `> ${client.emoji[badge]} **${badge.replace(
-                /([a-z])([A-Z])/g,
-                "$1 $2",
-              )}**`,
-            );
-          }
+      if (!data || !data.badge) {
+        return message.channel.send(`Oops! **${selectedUser.username}** has no badges.`);
+      }
+
+      for (const [badge, value] of Object.entries(data.badge)) {
+        if (value) {
+          cache.push(
+            `> ${client.emoji[badge] || ""} **${badge.replace(
+              /([a-z])([A-Z])/g,
+              "$1 $2"
+            )}**`
+          );
         }
       }
 
       if (cache.length === 0) {
-        cache.push(`Oops! **${selectedUser.username}** has no badges.`);
+        return message.channel.send(`Oops! **${selectedUser.username}** has no badges.`);
       }
 
-      const Removing = Object.keys(data.badge)
+      const removing = Object.keys(data.badge)
         .filter((badge) => data.badge[badge])
         .map((badge) => ({
           label: badge.replace(/([a-z])([A-Z])/g, "$1 $2"),
@@ -112,20 +124,18 @@ module.exports = {
           .setCustomId("remove-badge")
           .setPlaceholder("Select a badge to remove")
           .setMinValues(1)
-          .setMaxValues(Removing.length)
-          .addOptions(Removing),
+          .setMaxValues(removing.length)
+          .addOptions(removing)
       );
 
       const embed = new EmbedBuilder()
         .setColor(0x2f3136)
         .setAuthor({
           name: `${selectedUser.username}'s Profile`,
-          iconURL: `${selectedUser.displayAvatarURL({ dynamic: true })}`,
+          iconURL: selectedUser.displayAvatarURL({ dynamic: true }),
         })
         .setDescription(
-          `[${selectedUser.displayName}'s](${
-            client.config.links.support
-          }) Badges\n${cache.join("\n")}`,
+          `[${selectedUser.displayName}'s](${client.config.links.support}) Badges\n${cache.join("\n")}`
         )
         .setFooter({
           text: `Remove badges by selecting from the dropdown!`,
@@ -138,20 +148,18 @@ module.exports = {
         components: [row],
       });
 
-      const badgeCollector = await badgeMessage.createMessageComponentCollector(
-        {
-          filter: (i) => {
-            if (message.author.id === i.user.id) return true;
-            else {
-              i.reply({
-                content: `${client.emoji.cross} | That's not your session. Run \`${prefix}help\` to create your own.`,
-                ephemeral: true,
-              });
-            }
-          },
-          time: 60000,
+      const badgeCollector = badgeMessage.createMessageComponentCollector({
+        filter: (i) => {
+          if (message.author.id === i.user.id) return true;
+
+          i.reply({
+            content: `${client.emoji.cross} | That's not your session.`,
+            ephemeral: true,
+          });
+          return false;
         },
-      );
+        time: 60000,
+      });
 
       badgeCollector.on("collect", async (i) => {
         if (i.customId === "remove-badge") {
@@ -165,12 +173,12 @@ module.exports = {
 
             await memberData.save();
 
-            i.reply({
+            await i.reply({
               embeds: [
                 new EmbedBuilder()
                   .setColor(client.color)
                   .setDescription(
-                    `Removed **${badgesToRemove.length}** badges from <@${selectedUser.id}>`,
+                    `Removed **${badgesToRemove.length}** badges from <@${selectedUser.id}>`
                   ),
               ],
             });
