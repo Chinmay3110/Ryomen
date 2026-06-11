f55c4da602fdcfabbc06b256c5610813017bd17a
Xylus <105723951+Chinmay3110@users.noreply.github.com>
2026-06-11 13:57:05 +0530

    Update kill.js

--- src/commands/Fun/kill.js
+++ src/commands/Fun/kill.js
@@ -1,38 +1,54 @@
 const { EmbedBuilder } = require("discord.js");
-const axios = require("axios");
 
 module.exports = {
   name: "kill",
   aliases: ["murder"],
   category: "Fun",
   cooldown: 3,
+  description: "Kill someone",
+  args: true,
+  usage: "<user>",
 
   execute: async (message, args, client) => {
     const user = message.mentions.users.first();
 
-    if (!user)
-      return message.reply("Mention someone to kill.");
-
-    if (user.id === message.author.id)
-      return message.reply("You can't kill yourself.");
-
-    try {
-      const res = await axios.get("https://api.waifu.pics/sfw/kill");
-
-      const embed = new EmbedBuilder()
-        .setColor(client.color)
-        .setDescription(`🔪 ${message.author} killed ${user}`)
-        .setImage(res.data.url)
-        .setTimestamp();
-
-      return message.channel.send({ embeds: [embed] });
-
-    } catch (err) {
-      console.log(err);
-
-      return message.channel.send({
-        content: "Failed to fetch GIF."
+    if (!user) {
+      return message.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setColor(client.color)
+            .setDescription("❌ Please mention a user to kill."),
+        ],
       });
     }
+
+    if (user.id === message.author.id) {
+      return message.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setColor(client.color)
+            .setDescription("❌ You can't kill yourself."),
+        ],
+      });
+    }
+
+    const gifs = [
+      "https://i.imgur.com/8Km9tLL.gif",
+      "https://i.imgur.com/2X6nR9K.gif",
+      "https://i.imgur.com/B5QJ8zH.gif",
+      "https://i.imgur.com/W9WJ4Yk.gif"
+    ];
+
+    const gif = gifs[Math.floor(Math.random() * gifs.length)];
+
+    const embed = new EmbedBuilder()
+      .setColor(client.color)
+      .setDescription(`🔪 ${message.author} killed ${user}`)
+      .setImage(gif)
+      .setTimestamp();
+
+    return message.channel.send({
+      embeds: [embed],
+    });
   },
 };
