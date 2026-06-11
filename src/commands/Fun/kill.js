88c9a82b3e7931205f14afb6f60755b71aa69215
Xylus <105723951+Chinmay3110@users.noreply.github.com>
2026-06-11 13:36:24 +0530

    Update kill.js

--- src/commands/Fun/kill.js
+++ src/commands/Fun/kill.js
@@ -1,5 +1,5 @@
 const { EmbedBuilder } = require("discord.js");
-const { default: axios } = require("axios");
+const axios = require("axios");
 
 module.exports = {
   name: "kill",
@@ -10,33 +10,57 @@ module.exports = {
   args: true,
   usage: "<user>",
   owner: false,
-  execute: async (message, args, client, prefix) => {
+
+  execute: async (message, args, client) => {
     const user = message.mentions.users.first();
+
     if (!user) {
       return message.reply({
         embeds: [
           new EmbedBuilder()
             .setColor(client.color)
-            .setDescription(`Please mention a user to kill.`),
+            .setDescription("Please mention a user to kill."),
         ],
-      });
+      }).catch(() => null);
     }
+
     if (user.id === message.author.id) {
       return message.reply({
         embeds: [
           new EmbedBuilder()
             .setColor(client.color)
-            .setDescription(`You can't kill yourself.`),
+            .setDescription("You can't kill yourself."),
+        ],
+      }).catch(() => null);
+    }
+
+    try {
+      const images = [
+        "https://media.tenor.com/x8v1oNUOmg4AAAAC/anime-fight.gif",
+        "https://media.tenor.com/l6D9i4mP0GQAAAAC/anime-kill.gif",
+        "https://media.tenor.com/5nP6j4KJ3w4AAAAC/anime-attack.gif",
+        "https://media.tenor.com/e6z8s6BvY7AAAAAC/anime-sword.gif",
+      ];
+
+      const image = images[Math.floor(Math.random() * images.length)];
+
+      const embed = new EmbedBuilder()
+        .setColor(client.color)
+        .setDescription(`🔪 ${message.author} killed ${user}`)
+        .setImage(image)
+        .setTimestamp();
+
+      return message.channel.send({ embeds: [embed] });
+    } catch (err) {
+      console.error(err);
+
+      return message.channel.send({
+        embeds: [
+          new EmbedBuilder()
+            .setColor(client.color)
+            .setDescription("❌ Something went wrong."),
         ],
       });
     }
-    const response = await axios.get("https://api.waifu.pics/sfw/kill");
-    const image = response.data.url;
-    const embed = new EmbedBuilder()
-      .setColor(client.color)
-      .setDescription(`${message.author} Killed ${user}`)
-      .setImage(image)
-      .setTimestamp();
-    message.channel.send({ embeds: [embed] });
   },
 };
