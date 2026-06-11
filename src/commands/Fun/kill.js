142f94b2c1167a9134b087a1ae0a9468fabdd863
Xylus <105723951+Chinmay3110@users.noreply.github.com>
2026-06-11 13:41:48 +0530

    Update kill.js

--- src/commands/Fun/kill.js
+++ src/commands/Fun/kill.js
@@ -1,5 +1,4 @@
 const { EmbedBuilder } = require("discord.js");
-const axios = require("axios");
 
 module.exports = {
   name: "kill",
@@ -34,33 +33,23 @@ module.exports = {
       }).catch(() => null);
     }
 
-    try {
-      const images = [
-        "https://media.tenor.com/x8v1oNUOmg4AAAAC/anime-fight.gif",
-        "https://media.tenor.com/l6D9i4mP0GQAAAAC/anime-kill.gif",
-        "https://media.tenor.com/5nP6j4KJ3w4AAAAC/anime-attack.gif",
-        "https://media.tenor.com/e6z8s6BvY7AAAAAC/anime-sword.gif",
-      ];
+    const images = [
+      "https://i.imgur.com/8Km9tLL.gif",
+      "https://i.imgur.com/F6N0z.gif",
+      "https://i.imgur.com/yXOvdOS.gif",
+    ];
 
-      const image = images[Math.floor(Math.random() * images.length)];
+    const image = images[Math.floor(Math.random() * images.length)];
 
-      const embed = new EmbedBuilder()
-        .setColor(client.color)
-        .setDescription(`🔪 ${message.author} killed ${user}`)
-        .setImage(image)
-        .setTimestamp();
+    const embed = new EmbedBuilder()
+      .setColor(client.color)
+      .setDescription(`🔪 ${message.author} killed ${user}`)
+      .setImage(image)
+      .setTimestamp();
 
-      return message.channel.send({ embeds: [embed] });
-    } catch (err) {
-      console.error(err);
-
-      return message.channel.send({
-        embeds: [
-          new EmbedBuilder()
-            .setColor(client.color)
-            .setDescription("❌ Something went wrong."),
-        ],
-      });
-    }
+    return message.channel.send({
+      content: image,
+      embeds: [embed],
+    }).catch(() => null);
   },
 };
