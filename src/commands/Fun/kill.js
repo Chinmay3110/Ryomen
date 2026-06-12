d39e3b40d013b5e7ed67b81a9e63a28c287773c4
Xylus <105723951+Chinmay3110@users.noreply.github.com>
2026-06-13 01:21:55 +0530

    Update kill.js

--- src/commands/Fun/kill.js
+++ src/commands/Fun/kill.js
@@ -9,26 +9,22 @@ module.exports = {
   execute: async (message, args, client) => {
     const user = message.mentions.users.first();
 
-    if (!user)
-      return message.reply("Mention a user.");
-
+    if (!user) return message.reply("Mention a user.");
     if (user.id === message.author.id)
       return message.reply("You can't kill yourself.");
 
     const gifs = [
-      "https://media.tenor.com/QM6bncK9nXQAAAAC/anime-kill.gif",
-      "https://media.tenor.com/2roX3uxz_68AAAAC/anime-sword.gif",
-      "https://media.tenor.com/y0A6fHq7V7gAAAAC/anime-fight.gif"
+      "https://i.imgur.com/7l6QG8Y.gif",
+      "https://i.imgur.com/6RL6j7D.gif"
     ];
 
     const gif = gifs[Math.floor(Math.random() * gifs.length)];
 
     const embed = new EmbedBuilder()
-      .setColor(client.color || "#ff0000")
+      .setColor("#ff0000")
       .setTitle("💀 Kill")
       .setDescription(`🔪 **${message.author.username}** killed **${user.username}**`)
-      .setImage(gif)
-      .setTimestamp();
+      .setImage(gif);
 
     await message.channel.send({ embeds: [embed] });
   },
