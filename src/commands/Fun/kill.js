ceae64ad509804b5880d09b3b8c26204d83b7c92
Xylus <105723951+Chinmay3110@users.noreply.github.com>
2026-06-13 01:20:23 +0530

    Update kill.js

--- src/commands/Fun/kill.js
+++ src/commands/Fun/kill.js
@@ -16,19 +16,20 @@ module.exports = {
       return message.reply("You can't kill yourself.");
 
     const gifs = [
-      "https://c.tenor.com/Ug6CbVA1ZsMAAAAd/anime-fight.gif",
-      "https://c.tenor.com/8uP8M2p0kQ8AAAAd/anime-sword.gif",
-      "https://c.tenor.com/4bY0bJ5n8jQAAAAd/anime-attack.gif",
-      "https://c.tenor.com/3zzjM_k8aA4AAAAd/anime-kill.gif"
+      "https://media.tenor.com/QM6bncK9nXQAAAAC/anime-kill.gif",
+      "https://media.tenor.com/2roX3uxz_68AAAAC/anime-sword.gif",
+      "https://media.tenor.com/y0A6fHq7V7gAAAAC/anime-fight.gif"
     ];
 
     const gif = gifs[Math.floor(Math.random() * gifs.length)];
 
     const embed = new EmbedBuilder()
-      .setColor(client.color)
-      .setDescription(`🔪 ${message.author} killed ${user}`)
-      .setImage(gif);
+      .setColor(client.color || "#ff0000")
+      .setTitle("💀 Kill")
+      .setDescription(`🔪 **${message.author.username}** killed **${user.username}**`)
+      .setImage(gif)
+      .setTimestamp();
 
-    return message.channel.send({ embeds: [embed] });
+    await message.channel.send({ embeds: [embed] });
   },
 };
