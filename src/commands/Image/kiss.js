8b4f8089c6fcfba2ded6dc9662f7c7dd9a3e46dc
Xylus <105723951+Chinmay3110@users.noreply.github.com>
2026-06-13 01:36:14 +0530

    Update kiss.js

--- src/commands/Image/kiss.js
+++ src/commands/Image/kiss.js
@@ -1,5 +1,5 @@
-const { EmbedBuilder, MessageFlags, AttachmentBuilder } = require("discord.js");
-const { default: axios } = require("axios");
+const { EmbedBuilder } = require("discord.js");
+const axios = require("axios");
 
 module.exports = {
   name: "kiss",
@@ -7,34 +7,64 @@ module.exports = {
   category: "Image",
   cooldown: 3,
   botPermissions: ["SendMessages", "EmbedLinks"],
-  userPermissions: [],
-  execute: async (message, args, client, prefix) => {
+
+  execute: async (message, args, client) => {
     const user = message.mentions.users.first();
+
     if (!user) {
       return message.reply({
         embeds: [
           new EmbedBuilder()
             .setColor(client.color)
-            .setDescription(`Please mention a user to kiss.`),
+            .setDescription("❌ Please mention a user to kiss."),
         ],
       });
     }
+
+    if (user.bot) {
+      return message.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setColor(client.color)
+            .setDescription("❌ You can't kiss bots."),
+        ],
+      });
+    }
+
     if (user.id === message.author.id) {
       return message.reply({
         embeds: [
           new EmbedBuilder()
             .setColor(client.color)
-            .setDescription(`You can't kiss yourself.`),
+            .setDescription("❌ You can't kiss yourself."),
         ],
       });
     }
-    const response = await axios.get("https://api.waifu.pics/sfw/kiss");
-    const image = response.data.url;
-    const embed = new EmbedBuilder()
-      .setColor(client.color)
-      .setDescription(`${message.author} kisses ${user}`)
-      .setImage(image)
-      .setTimestamp();
-    message.channel.send({ embeds: [embed] });
+
+    try {
+      const { data } = await axios.get(
+        "https://api.waifu.pics/sfw/kiss"
+      );
+
+      const embed = new EmbedBuilder()
+        .setColor(client.color)
+        .setDescription(`💋 ${message.author} kisses ${user}`)
+        .setImage(data.url)
+        .setTimestamp();
+
+      return message.channel.send({
+        embeds: [embed],
+      });
+    } catch (err) {
+      console.error(err);
+
+      return message.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setColor("Red")
+            .setDescription("❌ Failed to fetch a kiss image."),
+        ],
+      });
+    }
   },
 };
