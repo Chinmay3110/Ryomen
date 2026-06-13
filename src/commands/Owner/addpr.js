432b87f63be2ad03c736484a22b22bee6d3ab61a
Xylus <105723951+Chinmay3110@users.noreply.github.com>
2026-06-13 18:53:03 +0530

    Update addpr.js

--- src/commands/Owner/addpr.js
+++ src/commands/Owner/addpr.js
@@ -1,257 +1,140 @@
-/** @format
- *
- * Arrkiii By Ozuma xd
- * © 2024 Arrkiii Development
- *
- */
-
 const {
   EmbedBuilder,
-  MessageFlags,
   ActionRowBuilder,
   StringSelectMenuBuilder,
-  SelectMenuComponent,
-  ActionRowComponent,
 } = require("discord.js");
+
 const Data = require("../../schema/badge");
 
 module.exports = {
   name: "addpr",
   category: "Owner",
   aliases: ["addbadges"],
-  description: "to see member profile",
+  description: "Add profile badges to a user",
   args: false,
-  usage: "",
+  usage: "<user>",
   userPerms: [],
   owner: false,
-  execute: async (message, args, client, prefix, player, guildData) => {
 
-
+  execute: async (message, args, client, prefix) => {
     if (!client.owner.includes(message.author.id)) {
-      return message.reply(`You Can't Use This Command!`);
+      return message.reply("You Can't Use This Command!");
     }
 
     const user =
       message.mentions.members.first() ||
-      message.guild.members.cache.get(args[0]);
+      await message.guild.members.fetch(args[0]).catch(() => null);
+
     if (!user) {
-      message.reply(`Plaese Mention A User`);
+      return message.reply("Please mention a user or give a valid user ID.");
     }
 
-    if (user) {
-      let persons = await Data.findOne({ userId: user.id });
-      if (!persons) {
-        persons = await Data.create({ userId: user.id });
+    let person = await Data.findOne({ userId: user.id });
+
+    if (!person) {
+      person = await Data.create({ userId: user.id });
+    }
+
+    if (!person.badge) person.badge = {};
+
+    const badgesList = [
+      { label: "Owner", value: "owner", emoji: client.emoji.owner },
+      { label: "Dev", value: "dev", emoji: client.emoji.dev },
+      { label: "Admin", value: "admin", emoji: client.emoji.admin },
+      { label: "Staff", value: "staff", emoji: client.emoji.staff },
+      { label: "Partner", value: "partner", emoji: client.emoji.partner },
+      { label: "Supporter", value: "supporter", emoji: client.emoji.supporter },
+      { label: "Sponsor", value: "sponsor", emoji: client.emoji.sponsor },
+      { label: "Ownerspecial", value: "ownerspecial", emoji: client.emoji.ownerspecial },
+      { label: "Specialone", value: "specialone", emoji: client.emoji.specialone },
+      { label: "Loveone", value: "loveone", emoji: client.emoji.loveone },
+      { label: "Vip", value: "vip", emoji: client.emoji.vip },
+      { label: "Friend", value: "friend", emoji: client.emoji.friend },
+      { label: "Gfriend", value: "gfriend", emoji: client.emoji.gfriend },
+      { label: "Bug", value: "bug", emoji: client.emoji.bug },
+      { label: "Noprefix", value: "noprefix", emoji: client.emoji.noprefix },
+      { label: "All", value: "all" },
+    ];
+
+    const embed = new EmbedBuilder()
+      .setColor(client.color || "#2b2d31")
+      .setTitle("__Select Badges__")
+      .setDescription(`Choose badges to add for ${user}.`);
+
+    const row = new ActionRowBuilder().addComponents(
+      new StringSelectMenuBuilder()
+        .setCustomId("badges")
+        .setPlaceholder("Select badges")
+        .setMinValues(1)
+        .setMaxValues(badgesList.length)
+        .addOptions(badgesList)
+    );
+
+    const msg = await message.channel.send({
+      embeds: [embed],
+      components: [row],
+    });
+
+    const collector = msg.createMessageComponentCollector({
+      filter: async (i) => {
+        if (i.user.id === message.author.id) return true;
+
+        await i.reply({
+          content: `${client.emoji.cross || "❌"} | That's not your session.`,
+          ephemeral: true,
+        });
+
+        return false;
+      },
+      time: 60000,
+    });
+
+    collector.on("collect", async (i) => {
+      await i.deferUpdate();
+
+      let memberData = await Data.findOne({ userId: user.id });
+
+      if (!memberData) {
+        memberData = await Data.create({ userId: user.id });
       }
 
-      const embed = new EmbedBuilder()
-        .setTitle("__Which 1>?__")
-        .addFields({
-          name: "Premium.?",
-          value:
-            "> _Creator,\n> Girlowner,\n> Owner,\n> Dev,\n> Admin,\n> Staff,\n> Partner,\n> Supporter,\n> Sponsor,_",
-          inline: true,
-        })
-        .addFields({
-          name: "For All.?",
-          value:
-            "> _Ownerspecial,\n> Specialone,\n> Loveone,\n> Vip,\n> Friend,\n> Gfriend,\n> Bug,\n> Noprefix_",
-          inline: true,
-        })
-        .setColor(client.color)
-        .setImage(client.config.links.arrkiii);
+      if (!memberData.badge) memberData.badge = {};
 
-      const main = new ActionRowBuilder().addComponents([
-        new StringSelectMenuBuilder()
-          .setCustomId("badges")
-          .setPlaceholder("Select badges")
-          .setMinValues(1)
-          .setMaxValues(10)
-          .addOptions(
-            { label: "Owner", value: "owner", emoji: client.emoji.owner },
-            { label: "Dev", value: "dev", emoji: client.emoji.dev },
-            { label: "Admin", value: "admin", emoji: client.emoji.admin },
-            { label: "Staff", value: "staff", emoji: client.emoji.staff },
-            { label: "Partner", value: "partner", emoji: client.emoji.partner },
-            {
-              label: "Supporter",
-              value: "supporter",
-              emoji: client.emoji.supporter,
-            },
-            { label: "Sponsor", value: "sponsor", emoji: client.emoji.sponsor },
-            {
-              label: "Ownerspecial",
-              value: "ownerspecial",
-              emoji: client.emoji.ownerspecial,
-            },
-            {
-              label: "Specialone",
-              value: "specialone",
-              emoji: client.emoji.specialone,
-            },
-            { label: "Loveone", value: "loveone", emoji: client.emoji.loveone },
-            { label: "Vip", value: "vip", emoji: client.emoji.vip },
-            { label: "Friend", value: "friend", emoji: client.emoji.friend },
-            { label: "Bug", value: "bug", emoji: client.emoji.bug },
-            { label: "All", value: "all" },
-          ),
-      ]);
+      let selected = i.values;
 
-      const msg = await message.channel.send({
-        embeds: [embed],
-        components: [main],
+      if (selected.includes("all")) {
+        selected = badgesList
+          .filter((b) => b.value !== "all")
+          .map((b) => b.value);
+      }
+
+      for (const badge of selected) {
+        memberData.badge[badge] = true;
+      }
+
+      memberData.markModified("badge");
+      await memberData.save();
+
+      const doneEmbed = new EmbedBuilder()
+        .setColor(client.color || "#2b2d31")
+        .setDescription(
+          `✅ Added **${selected.length}** badge(s) to ${user}:\n\`${selected.join("`, `")}\``
+        );
+
+      await msg.edit({
+        embeds: [doneEmbed],
+        components: [],
       });
 
-      const collector = await msg.createMessageComponentCollector({
-        filter: (i) => {
-          if (message.author.id === i.user.id) return true;
-          else {
-            i.reply({
-              content: `${client.emoji.corss} | That's not your session run : \`${prefix}help\` to create your own.`,
-              ephemeral: true,
-            });
-          }
-        },
-        time: 60000,
-      });
+      collector.stop();
+    });
 
-      const owner = new EmbedBuilder()
-        .setColor(client.color)
-        .setDescription(`Added ${client.emoji.music} badge to <@${user.id}>`);
-
-      const dev = new EmbedBuilder()
-        .setColor(client.color)
-        .setDescription(`Added ${client.emoji.dev} badge to <@${user.id}>`);
-
-      const admin = new EmbedBuilder()
-        .setColor(client.color)
-        .setDescription(`Added ${client.emoji.admin} badge to <@${user.id}>`);
-
-      const staff = new EmbedBuilder()
-        .setColor(client.color)
-        .setDescription(`Added ${client.emoji.staff} badge to <@${user.id}>`);
-
-      const partner = new EmbedBuilder()
-        .setColor(client.color)
-        .setDescription(`Added ${client.emoji.partner} badge to <@${user.id}>`);
-
-      const supporter = new EmbedBuilder()
-        .setColor(client.color)
-        .setDescription(
-          `Added ${client.emoji.supporter} badge to <@${user.id}>`,
-        );
-
-      const sponsor = new EmbedBuilder()
-        .setColor(client.color)
-        .setDescription(`Added ${client.emoji.sponsor} badge to <@${user.id}>`);
-
-      const ownerspecial = new EmbedBuilder()
-        .setColor(client.color)
-        .setDescription(
-          `Added ${client.emoji.musicspecial} badge to <@${user.id}>`,
-        );
-
-      const specialone = new EmbedBuilder()
-        .setColor(client.color)
-        .setDescription(
-          `Added ${client.emoji.specialone} badge to <@${user.id}>`,
-        );
-
-      const loveone = new EmbedBuilder()
-        .setColor(client.color)
-        .setDescription(`Added ${client.emoji.loveone} badge to <@${user.id}>`);
-
-      const vip = new EmbedBuilder()
-        .setColor(client.color)
-        .setDescription(`Added ${client.emoji.vip} badge to <@${user.id}>`);
-
-      const friend = new EmbedBuilder()
-        .setColor(client.color)
-        .setDescription(`Added ${client.emoji.friend} badge to <@${user.id}>`);
-
-      const gfriend = new EmbedBuilder()
-        .setColor(client.color)
-        .setDescription(`Added ${client.emoji.gfriend} badge to <@${user.id}>`);
-
-      const bug = new EmbedBuilder()
-        .setColor(client.color)
-        .setDescription(`Added ${client.emoji.bug} badge to <@${user.id}>`);
-
-      const noprefix = new EmbedBuilder()
-        .setColor(client.color)
-        .setDescription(
-          `Added ${client.emoji.noprefix} badge to <@${user.id}>`,
-        );
-
-      const all = {
-        color: 0x2f3136,
-        description: `Added ${client.emoji.music} ${client.emoji.dev} ${client.emoji.admin} ${client.emoji.staff} ${client.emoji.partner} ${client.emoji.supporter} ${client.emoji.sponsor} ${client.emoji.musicspecial} ${client.emoji.specialone} ${client.emoji.loveone} ${client.emoji.vip} ${client.emoji.friend} ${client.emoji.bug} badges to <@${user.id}>`,
-      };
-
-      collector.on("collect", async (i) => {
-        if (i.customId === "badges") {
-          const badges = i.values;
-          let Member = await Data.findOne({ userId: user.id });
-          if (!Member) {
-            Member = await Data.create({ userId: user.id });
-          }
-          for (const badge of badges) {
-            if (badge === "owner") {
-              Member.badge.owner = true;
-            } else if (badge === "dev") {
-              Member.badge.dev = true;
-            } else if (badge === "admin") {
-              Member.badge.admin = true;
-            } else if (badge === "staff") {
-              Member.badge.staff = true;
-            } else if (badge === "partner") {
-              Member.badge.partner = true;
-            } else if (badge === "supporter") {
-              Member.badge.supporter = true;
-            } else if (badge === "sponsor") {
-              Member.badge.sponsor = true;
-            } else if (badge === "ownerspecial") {
-              Member.badge.ownerspecial = true;
-            } else if (badge === "specialone") {
-              Member.badge.specialone = true;
-            } else if (badge === "loveone") {
-              Member.badge.loveone = true;
-            } else if (badge === "vip") {
-              Member.badge.vip = true;
-            } else if (badge === "friend") {
-              Member.badge.friend = true;
-            } else if (badge === "bug") {
-              Member.badge.bug = true;
-            } else if (badge === "noprefix") {
-              Member.badge.noprefix = true;
-            } else if (badge === "all") {
-              Member.badge.owner = true;
-              Member.badge.dev = true;
-              Member.badge.admin = true;
-              Member.badge.staff = true;
-              Member.badge.partner = true;
-              Member.badge.supporter = true;
-              Member.badge.sponsor = true;
-              Member.badge.ownerspecial = true;
-              Member.badge.specialone = true;
-              Member.badge.loveone = true;
-              Member.badge.vip = true;
-              Member.badge.friend = true;
-              Member.badge.bug = true;
-            }
-          }
-          await Member.save();
-          i.reply({
-            embeds: [
-              new EmbedBuilder()
-                .setColor(client.color)
-                .setDescription(
-                  `Added **${badges.length}** badges to <@${user.id}>`,
-                ),
-            ],
-          });
-        }
-      });
-    }
+    collector.on("end", async (_, reason) => {
+      if (reason === "time") {
+        await msg.edit({
+          components: [],
+        }).catch(() => null);
+      }
+    });
   },
 };
